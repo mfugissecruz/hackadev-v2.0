544a88df1c072ef5380d98daf3330c43b2f238f0
Marcelo Cruz <mfugissecruz@Marcelos-Mac-mini.local>
2022-10-06 23:45:59 -0300

    carrinho funcionando

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,13 +1,39 @@
 import styles from "./Product.module.css";
-import { FaHeart, FaCartPlus } from "react-icons/fa";
+import { FaCartPlus } from "react-icons/fa";
+
+import { addToCart } from '../../redux/ducks/cart/cartSlice'
+import { useDispatch } from "react-redux";
 
 export default function Product({ productInfo }) {
   const product = productInfo;
+  const dispatch = useDispatch();
+
+  const handleOpenSidebar = () => {
+    document.querySelector('#sidebar').classList.remove('sidebar--isHidden')
+  
+  }
+
+  const addItemInCart = () => {
+    handleOpenSidebar()
+    dispatch(
+      addToCart({
+        id: product.id,
+        title: product.product,
+        image:product.image,
+        price: product.price,
+      }),
+      
+    )
+  }
+
 
   return (
     <>
       <div className={styles.product}>
-        <button className={styles.addCart}>
+        <button 
+          className={styles.addCart}
+          onClick={addItemInCart}
+        >
           <FaCartPlus />
         </button>
         <a href={`products/${product.slug}/?color=${product.color}`}>
